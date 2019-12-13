653335adf9841490b0d77b65ed0069cd4cb11493
Enzo M <47283455+codeSourc3@users.noreply.github.com>
2019-12-13 03:25:48 -0600

    Base file structure
    placed some of the file infrastructure. Need to put the dependencies of the /bibliophile-core/pom.xml into their necessary modules.

---
+++ bibliophile-core/src/test/java/com/codeball/bibliophile/core/GenericEntityRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.codeball.bibliophile.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author enzoj
+ *
+ */
+public interface GenericEntityRepository extends JpaRepository<GenericEntity, Long> {
+
+}
